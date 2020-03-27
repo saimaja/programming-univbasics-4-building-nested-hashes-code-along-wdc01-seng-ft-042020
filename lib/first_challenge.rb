961a7bc17971b4c2dfd124162f33ea5316735346
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-27 19:26:30 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,3 @@
 def first_challenge
-   epic_tragedy = {montague: {patriarch:{}, matriarch: {}, hero: {}, hero_friends: []}, 
-                  capulet: {patriarch: {}, matriarch: {}, heroine: {}, heroine_friends: []
-                    
-                  }}
+   epic_tragedy = {montague: {}, capulet: {}}
 end
